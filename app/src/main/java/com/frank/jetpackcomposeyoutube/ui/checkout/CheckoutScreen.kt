caa00309bf21991a5fddd44bd7d46c0f7a4660a7
Nhat Vu <nhatvu@HomeMac.local>
2022-11-06 16:40:40 +0700

    add ui for checkout

--- app/src/main/java/com/frank/jetpackcomposeyoutube/ui/checkout/CheckoutScreen.kt
+++ app/src/main/java/com/frank/jetpackcomposeyoutube/ui/checkout/CheckoutScreen.kt
@@ -1,18 +1,29 @@
 package com.frank.jetpackcomposeyoutube.ui.checkout
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun CheckoutScreen(cartId: String, customerId: String){
- Column(modifier = Modifier.fillMaxSize().padding(24.dp)) {
+fun CheckoutScreen(
+    cartId: String, customerId: String,
+    placeOrderAction: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
 
-     Text("Processing cart with id: $cartId for customer with id: $customerId")
-
- }
+        Text("Processing cart with id: $cartId for customer with id: $customerId")
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = {
+            placeOrderAction()
+        }) {
+            Text(text = "Place order")
+        }
+    }
 }
--- app/src/main/java/com/frank/jetpackcomposeyoutube/ui/checkout/CheckoutSuccessScreen.kt
+++ app/src/main/java/com/frank/jetpackcomposeyoutube/ui/checkout/CheckoutSuccessScreen.kt
@@ -1,8 +1,44 @@
 package com.frank.jetpackcomposeyoutube.ui.checkout
 
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun CheckoutSuccessScreen(){
-
+fun CheckoutSuccessScreen(
+    goHomeAction: () -> Unit,
+    viewOrderDetailAction: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        Text("Thank you for your order!", style = MaterialTheme.typography.h4)
+        Spacer(modifier = Modifier.height(24.dp))
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.Bottom,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedButton(onClick = {
+                goHomeAction()
+            }, modifier = Modifier.weight(1f)) {
+                Text(text = "Go home")
+            }
+            Button(onClick = {
+                viewOrderDetailAction("123")
+            }, modifier = Modifier.weight(1f)) {
+                Text(text = "Check order")
+            }
+        }
+    }
 }
+
